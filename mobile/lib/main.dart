45c61d5a2cf10855ab91e5a12995892c6f23f661
Bruno <bruno260207@gmail.com>
2026-05-06 18:47:29 -0500

    Implementado el log in con token

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-import 'services/api_service.dart';
-import 'models/estacion.dart';
+import 'screens/login_screen.dart';
+import 'screens/home_page.dart';
+import 'services/auth_service.dart';
 
-void main() {
-  runApp(const SMATApp());
-}
+void main() => runApp(const SMATApp());
 
 class SMATApp extends StatelessWidget {
   const SMATApp({super.key});
@@ -13,72 +12,20 @@ class SMATApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-
-  late Future<List<Estacion>> futureEstaciones;
-
-  @override
-  void initState() {
-    super.initState();
-    futureEstaciones = ApiService().fetchEstaciones();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('SMAT - Monitoreo Móvil')),
-
-      body: FutureBuilder<List<Estacion>>(
-        future: futureEstaciones,
+      title: 'SMAT Mobile',
+      home: FutureBuilder<String?>(
+        future: AuthService().getToken(),
         builder: (context, snapshot) {
-
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          }
-
-          else if (snapshot.hasError) {
-            return const Center(child: Text('❌ Error de conexión'));
-          }
-
-          else {
-            final estaciones = snapshot.data!;
-
-            return ListView.builder(
-              itemCount: estaciones.length,
-              itemBuilder: (context, index) {
-                final est = estaciones[index];
-
-                return ListTile(
-                  leading: const Icon(Icons.satellite_alt),
-                  title: Text(est.nombre),
-                  subtitle: Text(est.ubicacion),
-                );
-              },
+            return const Scaffold(
+              body: Center(child: CircularProgressIndicator()),
             );
           }
+          if (snapshot.hasData && snapshot.data != null) {
+            return const HomePage();
+          }
+          return const LoginScreen();
         },
-      ),
-
-      // Bóton del reto
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            futureEstaciones = ApiService().fetchEstaciones();
-          });
-        },
-        child: const Icon(Icons.refresh),
       ),
     );
   }
